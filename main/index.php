cf8b253bf4de81ff2c7865ecfb9d99bce47df412
Ivona <129669807+ivonapashova@users.noreply.github.com>
2023-10-29 00:04:19 +0300

    Update index.php

---
+++ main/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>nz</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        .sidebar {
+            height: 100%;
+            width: 250px;
+            position: fixed;
+            top: 0;
+            left: 0;
+            background-color: #333;
+            padding-top: 20px;
+        }
+
+        .sidebar a {
+            padding: 15px 25px;
+            text-align: left;
+            text-decoration: none;
+            font-size: 20px;
+            color: white;
+            display: block;
+        }
+
+        .content {
+            margin-left: 250px;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+    </style>
+</head>
+<body>
+
+<div class="sidebar">
+    <a href="#section1">Section 1</a>
+    <a href="#section2">Section 2</a>
+    <a href="#section3">Section 3</a>
+</div>
+
+<div class="content">
+    <h1>zapovqdai vasile</h1>
+    <p>mn smash</p>
+
+    <section id="section1">
+        <h2>Section 1</h2>
+        <p>pyrwa sekciq</p>
+    </section>
+
+    <section id="section2">
+        <h2>Section 2</h2>
+        <p>vtora sekciq</p>
+    </section>
+
+    <section id="section3">
+        <h2>Section 3</h2>
+        <p>treta</p>
+    </section>
+</div>
+
+</body>
+</html>
